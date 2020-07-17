c710c7513c8aba2fc521f8280f18026a464aa362
Carlos Santos <carlossantos@MacBook-Pro.local>
2020-07-17 10:16:05 -0300

    Commit Inicial

---
+++ src/test/java/io/carlosepdsjava/pontointeligente/api/PontointeligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.carlosepdsjava.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontointeligenteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
